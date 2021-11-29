609f2b0a02761a64ea0d1fe8233db6e0fec1210a
AM0726Github <Margaryanalik7@gmail.com>
2021-11-28 22:21:15 -0800

    adding timer variable to set time for quiz game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
-
-
+// declaring variables by id of content
 var quizHeader = document.getElementById('questionDisplay');
 var quizParagraph = document.getElementById('explanation-paragraph');
-var startQuizBtnEl = document.getElementById('start-quiz'); // Start Quiz button Btn El
+var startQuizBtnEl = document.getElementById('start-quiz');
+var containerDiv = document.getElementById('Container');
+
+var score = 0;
+var timeLeft = 100;
 
 const myQuestions = [
     {
@@ -63,8 +63,26 @@ startQuizBtnEl.addEventListener("click", function() {
     startQuizBtnEl.style.display = 'none';
     quizHeader.style.display = 'none';
     quizParagraph.style.display = 'none';
+    //
+    score = 0; // Score is 0 again.
+    timeLeft = 100;
 
+    // variable to store our intervalID
+    let nIntervId;
 
+    function changetime() {
+        // check if already an interval has been set up
+        debugger;
+        if (!nIntervId) {
+          nIntervId = setInterval(changeText, 1000);
+        }
+    }
 
+    function changeText() {
+        var oElem = document.getElementById("timeLeft");
+        timeLeft = parseInt(timeLeft) -  1;
+        oElem.innerText = timeLeft;
+    }
 
+    changetime();
 });
